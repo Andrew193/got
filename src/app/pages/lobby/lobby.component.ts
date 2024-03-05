462f9e15bbc0a093760a7887fb33f795b120b2ad
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2024-03-05 21:17:09 +0200

    Create daily reward component

--- src/app/pages/lobby/lobby.component.ts
+++ src/app/pages/lobby/lobby.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {GameFieldComponent} from "../../components/game-field/game-field.component";
+import {DailyRewardComponent} from "../../components/daily-reward/daily-reward.component";
 
 @Component({
   selector: 'app-lobby',
   standalone: true,
-    imports: [
-        GameFieldComponent
-    ],
+  imports: [
+    GameFieldComponent,
+    DailyRewardComponent
+  ],
   templateUrl: './lobby.component.html',
   styleUrl: './lobby.component.scss'
 })
