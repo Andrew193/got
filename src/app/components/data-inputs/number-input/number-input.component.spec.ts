619b182f4351dbf505f1ea42dd5f77e3047922b7
Andrew193 <sadovinskiy2@gmail.com>
2025-09-27 17:18:33 +0300

    - Add tests to the number-input.component.spec.ts

--- src/app/components/data-inputs/number-input/number-input.component.spec.ts
+++ src/app/components/data-inputs/number-input/number-input.component.spec.ts
@@ -1,5 +1,71 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NumberInputComponent } from './number-input.component';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+
+@Component({
+  standalone: true,
+  imports: [ReactiveFormsModule, NumberInputComponent],
+  template: `
+    <form [formGroup]="form">
+      <app-number-input controlName="test" label="Test label" />
+    </form>
+  `,
+})
+class HostComponent {
+  form = new FormGroup({ test: new FormControl(0, { nonNullable: true }) });
+}
+
 describe('NumberInputComponent', () => {
-  it('should create', () => {
-    expect(true).toBeTruthy();
+  let hostComponent: HostComponent;
+  let loader: HarnessLoader;
+  let fixture: ComponentFixture<HostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HostComponent, NoopAnimationsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HostComponent);
+    hostComponent = fixture.componentInstance;
+
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
+    fixture.detectChanges();
+  });
+
+  it('NumberInputComponent should de created', () => {
+    expect(hostComponent).toBeTruthy();
+  });
+
+  it('NumberInputComponent should get correct initial state', async () => {
+    const formFieldHarness = await loader.getHarness(MatFormFieldHarness);
+    const inputHarness = await loader.getHarness(MatInputHarness);
+
+    const label = await formFieldHarness.getLabel();
+
+    expect(label).toBe('Test label');
+    expect(await inputHarness.getValue()).toBe('0');
+    expect(await inputHarness.getType()).toBe('number');
+  });
+
+  it('NumberInputComponent should update value', async () => {
+    const input = await loader.getHarness(MatInputHarness);
+
+    await input.setValue('New');
+
+    fixture.detectChanges();
+
+    expect(await input.getValue()).toBeFalsy();
+
+    //Real value
+    await input.setValue('35');
+    fixture.detectChanges();
+    expect(await input.getValue()).toBe('35');
   });
 });
